075fd37371e3cb742f4a6df1e57a0eea1bdf8cf6
Martin Gyhagen <martin.gyhagen@gmail.com>
2012-11-25 23:39:50 +0100

    ADd user microposts 2

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -14,7 +14,14 @@ class MicropostsController < ApplicationController
 	end
 
 	def destroy
-		@micropost = current_user.microposts.find_by_id(params[:id])
-		redirect_to root_url if @micropost.nil?
+		@micropost.destroy
+		redirect_to root_url
 	end
+
+	private
+
+		def correct_user
+			@micropost = current_user.microposts.find_by_id(params[:id])
+			redirect_to root_url if @micropost.nil?
+		end
 end
